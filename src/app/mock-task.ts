09973f961f3a34291fc28fb47200cd7258be4c89
AIT-4 <federicodurussel@outlook.com>
2022-06-27 22:22:48 -0300

    Se agrega mock de tareas y el interface de las mismas

---
+++ src/app/mock-task.ts
@@ -0,0 +1,27 @@
+import { Task } from "./Task"
+export const TASKS: Task[] = [
+    {
+        id:1,
+        text: "Terminar primar tarea",
+        day: 'Agosto 5 a las 12:00',
+        reminder: true
+    },
+    {
+        id:2,
+        text: "Hacer las compras",
+        day: 'Agosto 5 a las 17:00',
+        reminder: true
+    },
+    {
+        id:3,
+        text: "Investigar sobre Boostrap",
+        day: 'Agosto 5 a las 19:00',
+        reminder: false
+    },
+    {
+        id:4,
+        text: "Leer",
+        day: 'Agosto 5 a las 21:00',
+        reminder: false
+    },
+]
